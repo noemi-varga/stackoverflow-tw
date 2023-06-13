441a04b9de0cbd8b9311055fcde2dae110a46a31
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-06-13 10:18:17 +0200

    add TableStatements interface

---
+++ src/main/java/com/codecool/stackoverflowtw/initialize/TableStatements.java
@@ -0,0 +1,35 @@
+package com.codecool.stackoverflowtw.initialize;
+
+public interface TableStatements {
+    String QUESTION = """
+            DROP TABLE IF EXISTS question;
+            
+            CREATE TABLE question (
+            question_id SERIAL PRIMARY KEY,
+            question_detail varchar(255) NOT NULL,
+            user_id INTEGER,
+            date DEFAULT CURRENT_TIMESTAMP)
+            """;
+    String ANSWER= """
+            DROP TABLE IF EXISTS answer;
+            
+            CREATE TABLE answer (
+            answer_id SERIAL PRIMARY KEY,
+            answer_detail varchar(255),
+            question_id INTEGER,
+            user_id INTEGER,
+            date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+            """;
+
+
+    String USER= """
+            DROP TABLE IF EXISTS user;
+            
+            CREATE TABLE user (
+            user_id SERIAL PRIMARY KEY,
+            user_name varchar(255)) NOT NULL,
+            UNIQUE(user_name)
+            """;
+
+
+}
