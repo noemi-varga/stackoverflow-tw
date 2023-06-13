c5878e7eb43032ceb4b92960fb90308b6c69133b
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-06-13 20:59:01 +0200

    create AnswerService and @Bean

---
+++ src/main/java/com/codecool/stackoverflowtw/service/AnswerService.java
@@ -0,0 +1,40 @@
+package com.codecool.stackoverflowtw.service;
+
+import com.codecool.stackoverflowtw.controller.dto.AnswerDTO;
+import com.codecool.stackoverflowtw.dao.AnswersDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AnswerService {
+
+    private AnswersDAO answersDAO;
+
+    @Autowired
+    public AnswerService(AnswersDAO answersDAO) {
+        this.answersDAO = answersDAO;
+    }
+
+    public List<AnswerDTO> getAllAnswers(int id) {
+        answersDAO.sayHi();
+        return answersDAO.findAllByQuestionId(id);
+    }
+
+    public AnswerDTO getAnswerById(int id) {
+        answersDAO.sayHi();
+        return answersDAO.findOneById(id);
+    }
+
+    public boolean deleteAnswerById(int id) {
+        answersDAO.sayHi();
+        return answersDAO.findOneAndDelete(id);
+    }
+
+    public boolean addNewQuestion(AnswerDTO answer) {
+        answersDAO.sayHi();
+        return answersDAO.save(answer);
+    }
+}
+
