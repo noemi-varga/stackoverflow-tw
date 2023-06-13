53c430ca3c372a429e69096ca08ac98b0a2560cd
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-06-13 14:48:52 +0200

    database connection bugfix

--- src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDAO.java
@@ -1,5 +1,10 @@
 package com.codecool.stackoverflowtw.dao;
 
+import com.codecool.stackoverflowtw.controller.dto.QuestionDTO;
+
+import java.util.List;
+
 public interface QuestionsDAO {
     void sayHi();
+    List<QuestionDTO> findAll();
 }
