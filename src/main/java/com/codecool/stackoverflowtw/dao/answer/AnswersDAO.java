15f572ae49832143fc950b3ab43734045e707d01
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-06-14 13:12:08 +0200

    implement NewAnswerDTO into save method

--- src/main/java/com/codecool/stackoverflowtw/dao/answer/AnswersDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/answer/AnswersDAO.java
@@ -1,6 +1,7 @@
 package com.codecool.stackoverflowtw.dao.answer;
 
 import com.codecool.stackoverflowtw.controller.dto.answer.AnswerDTO;
+import com.codecool.stackoverflowtw.controller.dto.answer.NewAnswerDTO;
 
 import java.util.List;
 
@@ -8,6 +9,6 @@ public interface AnswersDAO {
     void sayHi();
     List<AnswerDTO> findAllByQuestionId(int id);
     AnswerDTO findOneById(int id);
-    boolean save(AnswerDTO answerDTO);
+    int save(NewAnswerDTO answerDTO);
     boolean findOneAndDelete(int id);
 }
