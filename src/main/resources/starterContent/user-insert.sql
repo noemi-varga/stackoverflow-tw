4f4e0bcdb43b58b681728883fc433e4ac92d8314
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-06-14 12:05:24 +0200

    create starter users

---
+++ src/main/resources/starterContent/user-insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (
+    user_name,
+    password,
+)
+VALUES('beanAddict96', '6beanzZz9'),
+      ('G0v3rnm3ntB4d', 'aNARChy666'),
+      ('xXSilly_GooseXx', 'rAnDoHeHe123'),
+      ('NoScope360', '360C0DG0D');
