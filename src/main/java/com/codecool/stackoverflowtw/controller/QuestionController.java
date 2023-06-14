63dec64e9519a1472ffe57313913dfbf4cbbb024
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-06-14 12:09:10 +0200

    recommit sort related changes (not mine - cache issues?)

--- src/main/java/com/codecool/stackoverflowtw/controller/QuestionController.java
+++ src/main/java/com/codecool/stackoverflowtw/controller/QuestionController.java
@@ -1,14 +1,13 @@
 package com.codecool.stackoverflowtw.controller;
 
-import com.codecool.stackoverflowtw.controller.dto.NewQuestionDTO;
-import com.codecool.stackoverflowtw.controller.dto.QuestionDTO;
+import com.codecool.stackoverflowtw.controller.dto.question.NewQuestionDTO;
+import com.codecool.stackoverflowtw.controller.dto.question.QuestionDTO;
 import com.codecool.stackoverflowtw.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequestMapping("questions")
 public class QuestionController {
@@ -26,32 +25,16 @@ public class QuestionController {
 
     @GetMapping("/{id}")
     public QuestionDTO getQuestionById(@PathVariable int id) {
-        return questionService.getQuestionById(id);
-    }
-
-    @GetMapping("/sort/title")
-    public List<QuestionDTO> sortByTitle() {
-        return questionService.sortByTitle();
-    }
-
-    @GetMapping("/sort/answerCount")
-    public List<QuestionDTO> sortByAnswerCount() {
-        return questionService.sortByAnswerCount();
-    }
-
-    @GetMapping("/sort/date")
-    public List<QuestionDTO> sortByDate() {
-        return questionService.sortByDate();
+        return null;
     }
 
     @PostMapping("/")
-    public boolean addNewQuestion(@RequestBody NewQuestionDTO question) {
-        //TODO return posted record!
-        return questionService.addNewQuestion(question);
+    public int addNewQuestion(@RequestBody NewQuestionDTO question) {
+        return 0;
     }
 
     @DeleteMapping("/{id}")
     public boolean deleteQuestionById(@PathVariable int id) {
-        return questionService.deleteQuestionById(id);
+        return false;
     }
 }
--- src/main/java/com/codecool/stackoverflowtw/service/QuestionService.java
+++ src/main/java/com/codecool/stackoverflowtw/service/QuestionService.java
@@ -1,12 +1,10 @@
 package com.codecool.stackoverflowtw.service;
 
-import com.codecool.stackoverflowtw.controller.dto.NewQuestionDTO;
-import com.codecool.stackoverflowtw.dao.QuestionsDAO;
-import com.codecool.stackoverflowtw.controller.dto.QuestionDTO;
+import com.codecool.stackoverflowtw.dao.question.QuestionsDAO;
+import com.codecool.stackoverflowtw.controller.dto.question.QuestionDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -20,30 +18,22 @@ public class QuestionService {
     }
 
     public List<QuestionDTO> getAllQuestions() {
+        questionsDAO.sayHi();
         return questionsDAO.findAll();
     }
 
     public QuestionDTO getQuestionById(int id) {
+        questionsDAO.sayHi();
         return questionsDAO.findOneById(id);
     }
 
     public boolean deleteQuestionById(int id) {
+        questionsDAO.sayHi();
         return questionsDAO.findOneAndDelete(id);
     }
 
-    public boolean addNewQuestion(NewQuestionDTO question) {
+    public boolean addNewQuestion(QuestionDTO question) {
+        questionsDAO.sayHi();
         return questionsDAO.save(question);
-    }
-
-    public List<QuestionDTO> sortByTitle() {
-        return questionsDAO.sortByTitle();
-    }
-
-    public List<QuestionDTO> sortByAnswerCount() {
-        return questionsDAO.sortByAnswerCount();
-    }
-
-    public List<QuestionDTO> sortByDate() {
-        return questionsDAO.sortByDate();
     }
 }
