6de69622b6bf88a0491bdedcec4a38ebc023239a
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-06-14 12:04:55 +0200

    create UserService and @Bean

---
+++ src/main/java/com/codecool/stackoverflowtw/service/UserService.java
@@ -0,0 +1,31 @@
+package com.codecool.stackoverflowtw.service;
+
+import com.codecool.stackoverflowtw.controller.dto.user.NewUserDTO;
+import com.codecool.stackoverflowtw.controller.dto.user.UserDTO;
+import com.codecool.stackoverflowtw.dao.user.UsersDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UsersDAO usersDAO;
+
+    @Autowired
+    public UserService(UsersDAO usersDAO) {
+        this.usersDAO = usersDAO;
+    }
+
+    public List<UserDTO> getAllUsers() {
+        return usersDAO.findAll();
+    }
+
+    public UserDTO getUserByUsername(String username) {
+        return usersDAO.findOneByUsername(username);
+    }
+
+    public UserDTO addNewUser(NewUserDTO newUserDTO) {
+        return usersDAO.findOneById(usersDAO.save(newUserDTO));
+    }
+}
